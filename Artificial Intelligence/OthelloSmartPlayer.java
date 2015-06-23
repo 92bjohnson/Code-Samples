a2820ac5d84f265771ae363fd59ce59ebc5cce3c
92bjohnson <92bjohnson@gmail.com>
2015-06-23 15:55:08 -0400

    Update OthelloSmartPlayer.java

--- Artificial Intelligence/OthelloSmartPlayer.java
+++ Artificial Intelligence/OthelloSmartPlayer.java
@@ -1,4 +1,8 @@
 /*
+	AI for a computer-controlled othello player
+*/
+
+/*
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
